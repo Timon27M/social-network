9242d3bd91ef255454cf7668b119d5ce599f0769
Timofey Pichugin <tpich87@gmail.com>
2024-04-06 12:12:13 +0300

    reload

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react';
-import styles from './Main.module.scss';
-import { getUser } from '../../store/actionCreaters';
-import { useDispatch } from '../../store/store';
+import { useEffect } from "react";
+import styles from "./Main.module.scss";
+import { getUser } from "../../store/actionCreaters";
+import { useDispatch } from "../../store/store";
+import Navbar from "../../components/navbar/Navbar";
 
 function Main() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getUser());
-    }, []) 
-    
-    return (
-        <div>ОСНОВНАЯ СТРАНИЦА</div>
-    )
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  return (
+    <div className={`${styles.main}`}>
+      <div>
+        <Navbar />
+      </div>
+      <div></div>
+      <p className={`${styles.text}`}>
+        ОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ
+        СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ
+        СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ
+        СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ СТРАНИЦАОСНОВНАЯ
+        СТРАНИЦА
+      </p>
+    </div>
+  );
 }
 
 export default Main;
